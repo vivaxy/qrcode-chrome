99abcfe2a4144da37f235a915dbcc94224881557
vivaxy <xyxuye2007@126.com>
2015-08-22 12:49:56 +0800

    resolved #1

--- dest/js/index.js
+++ dest/js/index.js
@@ -2,76 +2,37 @@
  * @since 150215 14:31
  * @author vivaxy
  */
-var utf16to8 = function (str) {
-        var out, i, len, c;
-        out = '';
-        len = str.length;
-        for (i = 0; i < len; i++) {
-            c = str.charCodeAt(i);
-            if ((c >= 0x0001) && (c <= 0x007F)) {
-                out += str.charAt(i);
-            } else if (c > 0x07FF) {
-                out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-                out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
-                out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
-            } else {
-                out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
-                out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
-            }
-        }
-        return out;
-    },
 
-    $url = $('#url'),
-    $qr0 = $('#qr0'),
-    $qr1 = $('#qr1'),
-    $schema0 = $('#schema0'),
-    $schema1 = $('#schema1'),
-
-    generateQr = function () {
-        // TODO use redirect for wechat to open in other app
-        var schema0 = $schema0.val(),
-            schema1 = $schema1.val(),
-            url = $url.val();
-        $qr0.html('').qrcode({
-            // encode url
-            text: utf16to8(schema0 + (schema0 === '' ? url : encodeURIComponent(url)))
-        });
-        $qr1.html('').qrcode({
-            text: utf16to8(schema1 + (schema1 === '' ? url : encodeURIComponent(url)))
-        });
-        try {
-            chrome.storage.sync.set({'schema': [schema0, schema1]}, function () {
-                // Notify that we saved.
+var defaultSchemaList = ['', 'dianping://web?url='],
+    urlInput = document.querySelector('#url'),
+    init = function (schemaList) {
+        return Array.prototype.map.call(document.querySelectorAll('.qr-each'), function (qrEach, index) {
+            return new QrModel({
+                container: qrEach,
+                urlInput: urlInput,
+                schema: schemaList[index]
             });
-        } catch (e) {
-            console.log(e);
-        }
+        });
     };
-
-$url.on('keyup', generateQr);
-$schema0.on('keyup', generateQr);
-$schema1.on('keyup', generateQr);
 
 try {
     // init input data
     chrome.storage.sync.get('schema', function (storage) {
         if (storage.schema === undefined) {
-            storage.schema = ['', 'dianping://web?url='];
+            storage.schema = defaultSchemaList;
         }
-        $schema0.val(storage.schema[0]);
-        $schema1.val(storage.schema[1]);
-
+        init(storage.schema);
         // generate url and qr code
         chrome.tabs.getSelected(null, function (tab) {
-            $url.val(tab.url);
-            $url.trigger('keyup');
+            urlInput.value = tab.url;
+            var event = new Event('keyup');
+            urlInput.dispatchEvent(event);
         });
     });
 } catch (e) {
-    console.log(e);
+    init(defaultSchemaList);
 }
 
 setTimeout(function () {
-    $('body').removeClass('pull-up');
+    document.body.classList.remove('pull-up');
 }, 0);
---
+++ dest/js/lib/qr-model.js
@@ -0,0 +1,40 @@
+/**
+ * @since 15-08-22 11:55
+ * @author vivaxy
+ */
+'use strict';
+var QrModel = function (options) {
+    var _this = this;
+    this.container = options.container;
+    this.urlInput = options.urlInput;
+    this.schema = options.schema;
+    this.schemaInput = this.container.querySelector('input');
+    this.qrCodeContainer = this.container.querySelector('.qr');
+
+    this.schemaInput.value = this.schema;
+    this.qrcode = new QRCode(this.qrCodeContainer, {
+        text: this.getText(),
+        width: 240,
+        height: 240,
+        colorDark: 'rgba(0, 0, 0, 1)',
+        colorLight: 'rgba(0, 0, 0, 0)'
+    });
+    this.urlInput.addEventListener('keyup', _this.update.bind(_this), false);
+    this.schemaInput.addEventListener('keyup', function () {
+        _this.schema = _this.schemaInput.value;
+        _this.update.call(_this);
+    }, false);
+};
+
+QrModel.prototype = {
+    constructor: QrModel,
+    update: function () {
+        this.qrcode.makeCode(this.getText());
+        return this;
+    },
+    getText: function () {
+        var url = this.urlInput.value,
+            precessedUrl = this.schema === '' ? url : encodeURIComponent(url);
+        return this.schema + precessedUrl;
+    }
+};
